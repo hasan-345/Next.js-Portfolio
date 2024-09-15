9b215a4136b94eea025f8c849c9c918a56b01dce
hasan-345 <ahtishamhassan345@gmail.com>
2024-09-15 03:11:53 -0700

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
 
 
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-9xl w-full px-3">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto">
+      <div className="max-w-[1600px] mx-auto w-full px-3">
         <FloatingNav navItems={navItems} />
         <Hero/>
         <Grid/>
--- src/components/ui/FloatingNav.tsx
+++ src/components/ui/FloatingNav.tsx
@@ -57,7 +57,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto w-full border border-white/[0.2] rounded-md  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-5 bg-black-100 py-4  items-center justify-center space-x-4",
+          "flex max-w-fit sm:block hidden fixed top-10 inset-x-0 mx-auto w-full border border-white/[0.2] rounded-md  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-5 bg-black-100 py-4  items-center justify-center space-x-4",
           className
         )}
       >
@@ -69,8 +69,7 @@ export const FloatingNav = ({
               "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
           >
-            <span className="">{navItem.icon}</span>
-            <span className="">{navItem.name}</span>
+            <span className="sm:block hidden">{navItem.name}</span>
           </Link>
         ))}
        
